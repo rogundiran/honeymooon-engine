20124bc27aa7ac49b8954b89c59f97875882fb33
Ralphmore <raphealobairekanmi@gmail.com>
2026-04-07 01:32:23 +0100

    Delete the-honeymooner-core-v1.1.0 directory

--- the-honeymooner-core-v1.1.0/the-honeymooner-core/includes/helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('ABSPATH')) {
-    exit;
-}
-
-function hm_get_array_meta($post_id, $key) {
-    $value = get_post_meta($post_id, $key, true);
-    return is_array($value) ? $value : [];
-}
